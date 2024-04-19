e68f286af0413a3c2e7ad547be9be5eb56bd31b0
ygsiro <entoyukari@gmail.com>
2024-04-19 20:42:28 +0900

    detail

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'books'
 urlpatterns = [
     path('', views.list_books, name='list_books'),
+    path('<str:book_id>', views.detail_book, name='detail_books'),
 ]
--- apps/books/views.py
+++ apps/books/views.py
@@ -12,3 +12,16 @@ def list_books(request):
     }
     template = loader.get_template('books/list_books.html')
     return HttpResponse(template.render(context, request))
+
+def detail_book(request, book_id):
+    try:
+        book = Book.objects.get(book_id=book_id)
+    except Book.DoesNotExist:
+        book = None
+
+    context = {
+        'title': 'Detail Book',
+        'book': book,
+    }
+    template = loader.get_template('books/detail_book.html')
+    return HttpResponse(template.render(context, request))
