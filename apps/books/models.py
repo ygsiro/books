aad1152e2c092e836a306b01eae1789c64736724
ygsiro <entoyukari@gmail.com>
2024-04-19 19:53:55 +0900

    books

--- apps/books/models.py
+++ apps/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=32)
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+
+    def __str__(self) -> str:
+        return self.title
