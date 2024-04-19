111c0cc79c663880f348fcf8a14ccf17e9bbbc29
ygsiro <entoyukari@gmail.com>
2024-04-19 20:28:15 +0900

    layout

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Book
 
 # Create your views here.
 
 def list_books(request):
-    return HttpResponse("Hello world!")
+    books = Book.objects.all()
+    context = {
+        'title': 'List Books',
+        'books': books
+    }
+    template = loader.get_template('books/list_books.html')
+    return HttpResponse(template.render(context, request))
